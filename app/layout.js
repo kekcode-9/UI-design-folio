5e2fa3fdedf93f0b1443b5704137f517d4e173e6
kekcode-9 <rupamita.s97@gmail.com>
2023-11-18 12:32:00 +0530

    initial

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Gotu } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const gotu = Gotu({ subsets: ["latin"], weight: ["400"] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={gotu.className}>{children}</body>
     </html>
   )
 }
